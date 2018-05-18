9f027f8d127958dbc066876cb99d03fb6e981477
Balakrishna Ragala <baluragala@gmail.com>
2018-05-18 14:53:41 +0530

    Update index.js

--- www/index.js
+++ www/index.js
@@ -1,4 +1,3 @@
-var assert = require('nanoassert')
 module.exports = new DownloadManager()
 
 function DownloadManager () {
@@ -7,23 +6,6 @@ function DownloadManager () {
 DownloadManager.prototype.enqueue = function(req, cb) {
   if (typeof req === 'string') req = {uri: req}
   if (req.url) req.uri = req.url
-
-  assert(typeof req.uri === 'string', 'req.uri must be string')
-  assert(cb == null ? true : typeof cb === 'function', 'cb must be function')
-
-  assert(req.title == null ? true : typeof req.title === 'string', 'req.title must be string')
-  assert(req.description == null ? true : typeof req.description === 'string', 'req.description must be string')
-  assert(req.mimeType == null ? true : typeof req.mimeType === 'string', 'req.mimeType must be string')
-
-  assert(req.destinationInExternalFilesDir == null ? true : typeof req.destinationInExternalFilesDir.dirType === 'string', 'req.destinationInExternalFilesDir.dirType must be string')
-  assert(req.destinationInExternalFilesDir == null ? true : typeof req.destinationInExternalFilesDir.subPath === 'string', 'req.destinationInExternalFilesDir.subPath must be string')
-  assert(req.destinationInExternalPublicDir == null ? true : typeof req.destinationInExternalPublicDir.dirType === 'string', 'req.destinationInExternalPublicDir.dirType must be string')
-  assert(req.destinationInExternalPublicDir == null ? true : typeof req.destinationInExternalPublicDir.subPath === 'string', 'req.destinationInExternalPublicDir.subPath must be string')
-  assert(req.destinationUri == null ? true : typeof req.destinationUri === 'string', 'req.destinationUri must be string')
-
-  assert(req.visibleInDownloadsUi == null ? true : typeof req.visibleInDownloadsUi === 'boolean', 'req.visibleInDownloadsUi must be boolean')
-  assert(req.notificationVisibility == null ? true : Number.isSafeInteger(req.notificationVisibility), 'req.notificationVisibility must be safe integer')
-
   exec('enqueue', [req], cb)
 }
 
@@ -41,9 +23,6 @@ DownloadManager.prototype.query = function(filter, cb) {
 }
 
 DownloadManager.prototype.remove = function(ids, cb) {
-  assert(Array.isArray(ids), 'ids must be array')
-  assert(cb == null ? true : typeof cb === 'function', 'cb must be function')
-
   exec('remove', ids, cb)
 }
 
